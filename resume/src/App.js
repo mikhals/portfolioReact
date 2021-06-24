644dd35380dccef0d61e1db79162f3617a594a39
Mike <muhdikhlas017@gmail.com>
2021-06-24 23:40:51 +0900

    .

--- resume/src/App.js
+++ resume/src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import {ChromePicker} from "react-color"
 
 function App() {
+
+	const [text,setText] = useState("Enter text here!");
+	const [color,setColor] = useState("blue");
+
+	function textChange(e){
+		setText(e.target.value);
+		console.log(e);
+	}
+
+	function changeColor(c,e){
+		console.log(c.hex);
+		setColor(c.hex);
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Here
-        </a>
-      </header>
+			<h1 className="bigText"
+				style={{color:color}}
+				>{text}</h1>
+			<h2>{color}</h2>
+			<ChromePicker
+				onChange={changeColor}
+				color={color}/>
+      <textarea
+				value={text}
+				onChange={textChange}
+				/>
     </div>
   );
 }
